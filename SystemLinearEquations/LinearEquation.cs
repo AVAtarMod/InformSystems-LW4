e63b02329b4f2617c4f213edb33ba335df5d3019
AVAtarMod <Respawner289@gmail.com>
2022-04-21 22:41:06 +0300

    Add project SystemLinearEquation and tests

---
+++ SystemLinearEquations/LinearEquation.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LinearEquation
+{
+    public class LinearEquation
+    {
+    }
+}
---
+++ SystemLinearEquationsTest/SystemLinearEquationTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SystemLinearEquationTest
+{
+    [TestClass]
+    public class SystemLinearEquationTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+    [TestClass]
+    public class LinearEquationTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
