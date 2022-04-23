1211b4ac7b4a83023e9679bdad346715318a311b
AVAtar Mod <respawner289@gmail.com>
2022-04-23 13:25:05 +0300

    Migrate to linux workspace

--- IndexerTest/Properties/AssemblyInfo.cs
+++ IndexerTest/Properties/AssemblyInfo.cs
@@ -2,19 +2,19 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("IndexerTest")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("IndexerTest")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+// [assembly: AssemblyTitle("IndexerTest")]
+// [assembly: AssemblyDescription("")]
+// [assembly: AssemblyConfiguration("")]
+// [assembly: AssemblyCompany("")]
+// [assembly: AssemblyProduct("IndexerTest")]
+// [assembly: AssemblyCopyright("Copyright ©  2022")]
+// [assembly: AssemblyTrademark("")]
+// [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("841bf116-914d-4988-a752-75c7a80e85a0")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// [assembly: AssemblyVersion("1.0.0.0")]
+// [assembly: AssemblyFileVersion("1.0.0.0")]
