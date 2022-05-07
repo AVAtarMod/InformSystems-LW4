c9ead4fcff6e37548c58c00e735f8c59b2f30f20
AVAtar Mod <respawner289@gmail.com>
2022-05-08 01:45:57 +0300

    Add project Extensions Project contain: - List<T> extension EList<T> class - extension methods

---
+++ Extensions/Extensions.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace Extensions
+{
+    public class EList<T> : List<T>
+    {
+        public EList() : base()
+        {
+
+        }
+        public EList(int capacity) : base(capacity)
+        {
+
+        }
+        public EList(IEnumerable<T> collection) : base(collection)
+        {
+
+        }
+        public override string ToString()
+        {
+            string result = "";
+            const string separator = ", ";
+            foreach (T item in this)
+            {
+                result += $"{item.ToString()}{separator}";
+            }
+            return result.Remove(result.Length - separator.Length);
+        }
+    }
+    public static class Extensions
+    {
+        public static EList<T> ToEList<T>(this List<T> list)
+        {
+            return (EList<T>)list;
+        }
+        public static EList<T> ToEList<T>(this IEnumerable<T> collection)
+        {
+            return (EList<T>)collection;
+        }
+        public static EList<T> ToEList<T>(this T[] array)
+        {
+            return new EList<T>(array);
+        }
+    }
+}
